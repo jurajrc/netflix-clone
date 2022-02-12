16ae49697ae7dd190d895ef9b8f6bbf8e54ca2c0
jurajrc <81872592+jurajrc@users.noreply.github.com>
2022-02-13 00:15:57 +0100

    8. import components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import React, { useState } from "react";
+//Components
+import Header from "./components/Header";
+import NotLoggedIn from "./Pages/NotLoggedIn";
+
+// Style
+import GlobalStyle from "./components/GlobalStyle";
+import Footer from "./components/Footer";
 
 
 function App() {
   return (
     <div className="App">
-     
+      <GlobalStyle />
+      <Header />
+      <NotLoggedIn />
+
+      <Footer />
     </div>
   );
 }
