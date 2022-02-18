cfd0b776c8102fa200cfa2f35658acccd73b8056
jurajrc <81872592+jurajrc@users.noreply.github.com>
2022-02-18 08:45:49 +0100

    25 .add animation for detailMovie

--- src/animations.js
+++ src/animations.js
@@ -22,8 +22,72 @@ export const fadeIn = {
     show: {
         opacity: 1,
         transition: {
+            duration: 0.5,
+            when: "beforeChildren",
+            staggerChildren: 0.25,
+        }
+    }
+
+} 
+export const fadeInChildren = {
+    hidden: {
+        opacity: 0,
+        scale: 1.1
+    },
+    show: {
+        opacity: 1,
+        scale: 1,
+        transition: {
             duration: 0.5
         }
     }
 
 } 
+
+export const containerMovies = {
+    hidden: {
+        opacity: 0,
+    },
+    show: {
+        opacity: 1,
+        transition: {
+            staggerChildren: 0.1,
+            delayChildren: 0.3,
+        },
+    },
+    exit: {
+        opacity: 1,
+    }
+}
+export const movieItem = {
+    hidden: {
+        opacity: 0,
+        scale: 0.5,
+    },
+    show: {
+        opacity: 1,
+        scale: 1,
+    }
+}
+
+// Detail Movie
+export const detailMovieAnime = {
+    hidden: {
+        opacity: 0,
+        x: "-100%",
+    },
+    show: {
+        opacity: 1,
+        x: "0%",
+        transition: {
+            duration: 0.5,
+        },
+    },
+    exit: {
+        opacity: 0,
+        x: "-100%",
+        transition: {
+            duration: 0.75,
+        },
+    },
+}
