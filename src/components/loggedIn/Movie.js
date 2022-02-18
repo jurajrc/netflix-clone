3204095840267cd7bf180b03b902d0d135c174dd
jurajrc <81872592+jurajrc@users.noreply.github.com>
2022-02-18 08:56:47 +0100

    35. add @media Movie.js &

--- src/components/loggedIn/Movie.js
+++ src/components/loggedIn/Movie.js
@@ -1,37 +1,54 @@
 import React from 'react'
 import styled from 'styled-components'
-import { motion } from 'framer-motion'
 import logo from '../../assects/Logged_In/netflix-logo-small.svg'
+import { motion } from 'framer-motion'
+import { movieItem } from '../../animations'
 
-const Movie = ({ movie }) => {
+const Movie = ({ movie, handleDetail }) => {
+    
+
+    const handleId = (id) => {
+        handleDetail(id)
+    }
+
+    
+
   return (
-    <StyleMovie>
-        <h4>{movie.title}</h4>
-        <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
+    <StyleMovie variants={movieItem} onClick={() => handleId(movie.id)} >
+    
+        <motion.img whileHover={{ scale: 1.1 }} src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`} alt={movie.title} width="300px" height="450px" />
         <div className="logo">
-            <img src={logo} alt="logo" />
+            <motion.img src={logo} alt="logo" title={movie.id} />
         </div>
+           
     </StyleMovie>
   )
 }
 const StyleMovie = styled(motion.div)`
-    //width: 200px;
+    //width: 150px;
+    //height: auto;
     color: white;
     position: relative;
-    h4 {
-        font-size: 0.8em;
-        position: absolute;
-        top: 0;
-        width: 100%;
-        height: 3em;
-        padding: 5px 10px;
-        background: rgba(0, 0, 0, 0.4);
+    cursor: pointer;
+    
+    
+    img {
+        border-radius: 0.3em;
+        object-fit: cover;
     }
+
     .logo {
         position: absolute;
         left: 0;
         bottom: 0;
         width: 2em;
+        img {
+            width: 100%;
+            height: 100%;
+        }
+        @media (max-width: 550px) {
+            width: 1.5em;
+        }
     }
 `
 
