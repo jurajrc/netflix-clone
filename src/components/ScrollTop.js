305f1b1f1acc11a7170fec318439b4d22e58fd6a
jurajrc <81872592+jurajrc@users.noreply.github.com>
2022-02-18 08:55:25 +0100

    34. create ScrollTop.js

---
+++ src/components/ScrollTop.js
@@ -0,0 +1,20 @@
+import { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
+
+const ScrollTop = () => {
+
+    const {pathname} = useLocation()
+    useEffect(() => {
+      window.scroll({
+          top: 0,
+          left: 0,
+          bahavior: 'auto'
+      })
+    }, [pathname])
+
+  return (
+    null
+  )
+}
+
+export default ScrollTop
